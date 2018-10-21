43647f945d61b8933809f7d249aa08706331b03b
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2018-10-21 22:53:01 +0200

    Added general utilities to js code and template.

---
+++ betterweather/static/js/utilities.js
@@ -0,0 +1,15 @@
+function toCelsius(temperature) {
+    return temperature - 273.15;
+}
+
+function toFahrenheit(temperature) {
+    return temperature * 9/5 - 459.67;
+}
+
+function toKmph(mps) {
+    return mps * 3.6;
+}
+
+function toMph(mps) {
+    return mps * 2.2367;
+}
