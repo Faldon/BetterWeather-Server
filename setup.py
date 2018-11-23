0ac678cdfcac0ba3d9f687af92c461f17b669590
faldon <t.pulzer@thesecretgamer.de>
2018-11-23 19:15:48 +0100

    Added Manifest and extended setup information for initial public release.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name='BetterWeather',
     packages=['betterweather'],
+    version="1.0",
     include_package_data=True,
+    zip_safe=False,
     install_requires=[
         'flask', 'click',
     ],
